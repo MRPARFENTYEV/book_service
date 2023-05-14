bdc695e462c5c759225cc2057cee64893a769281
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-05-14 21:53:45 +0300

    uncommented

---
+++ insert_data.py
@@ -0,0 +1,5 @@
+session.add_all([publisher1,book1,shop1,stock1,sale1])
+session.add_all([publisher2,book2,stock2,sale2])
+session.add_all([shop3,stock3,sale3])
+session.add_all([book4,shop4,stock4,sale4])
+session.add_all([stock5,sale5])
--- main.py
+++ main.py
@@ -21,7 +21,6 @@ book2 = Book(title="Руслан и Людмила", publisher=publisher2)
 stock2 = Stock(count=800, book=book2, shop=shop1)
 sale2 = Sale(price=500, date_sale='08-11-2022', stock=stock2, count=55)
 
-
 shop3 = Shop(name="Лабиринт")
 stock3 = Stock(count=800, book=book1, shop=shop3)
 sale3 = Sale(price=580, date_sale='05-11-2022', stock=stock3, count=5)
@@ -32,53 +31,36 @@ stock4 = Stock(count=432, book=book4, shop=shop4)
 sale4 = Sale(price=490, date_sale='02-11-2022', stock=stock4, count=500)
 
 stock5 = Stock(count=580, book=book1, shop=shop1)
-sale5 = Sale(price= 600, date_sale='26-10-2022', stock=stock4, count=590)
-
-session.add_all([publisher1,book1,shop1,stock1,sale1]) '
-session.add_all([publisher2,book2,stock2,sale2])
-session.add_all([shop3,stock3,sale3])
-session.add_all([book4,shop4,stock4,sale4])
-session.add_all([stock5,sale5])
+sale5 = Sale(price=600, date_sale='26-10-2022', stock=stock4, count=590)
 
 
-def find_book_title_shop_name_sale_date_by_publisher_name():
-    publisher_name = input("Издатель:")
 
-    for book_title in session.query(Book).join(Publisher).filter(Publisher.name.like(publisher_name)).all():
-        print(book_title)
 
-    for Shop_name in session.query(Shop).join(Stock.shop).join(Book).join(Publisher).filter(
-            Publisher.name.like(publisher_name)).all():
-        print(Shop_name)
-    for sale_date in session.query(Sale).join(Stock).join(Book).join(Publisher).filter(
-                Publisher.name.like(publisher_name)).all():
+def find_data_by_name():
+    pub_name = input("Имя издателя:")
+    for books in session.query(Book.title, Publisher.name, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(
+            Stock).join(Sale).join(Shop).filter(Publisher.name.like(pub_name)).all():
+        print(' '.join(str(book) for book in books))
 
-        print(sale_date)
 
-def find_book_title_shop_name_sale_date_by_publisher_id():
-    publisher_id = input("Id Издателя:")
+def find_data_by_id():
+    pub_id = input("Id Издателя:")
 
-    for book_title in session.query(Book).join(Publisher).filter(Publisher.id == publisher_id).all():
-        print(book_title)
+    for books in session.query(Book.title, Publisher.name, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(
+            Stock).join(Sale).join(Shop).filter(Publisher.id == pub_id).all():
+        print(' '.join(str(book) for book in books))
 
-    for Shop_name in session.query(Shop).join(Stock.shop).join(Book).join(Publisher).filter(
-            Publisher.id == publisher_id).all():
-        print(Shop_name)
-    for sale_date in session.query(Sale).join(Stock).join(Book).join(Publisher).filter(
-            Publisher.id == publisher_id).all():
 
-        print(sale_date)
-#
+
 def how_do_you_want_to_search():
-    inputable_data = input('Вы хотите искать данные по Имени издателя или по id?')
+    inputable_data = input('Вы хотите искать данные по имени издателя или по id?')
     if inputable_data == 'id':
-        print(find_book_title_shop_name_sale_date_by_publisher_id())
+        find_data_by_id()
     else:
-        print(find_book_title_shop_name_sale_date_by_publisher_name)
+        find_data_by_name()
+
 
 how_do_you_want_to_search()
-
-
 
 session.commit()
 
